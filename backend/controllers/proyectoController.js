6e05696eea1a43917bea5dccb60a98eeb8272afd
MaximilianoTRZ <maximilianoriverozuin@gmail.com>
2023-02-07 19:41:45 -0300

    #448 Controlador de proyecto con ABM finalizado

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -8,7 +8,7 @@ const nuevoProyecto = async (req, res) => {
 
     const nuevoProyecto = new Proyecto(req.body)
     nuevoProyecto.creador = req.usuario._id // Le asignamos el usuario autenticado como creador del proyecto
-    
+
     try {
        const proyectoGuardado = await nuevoProyecto.save() // guardamos en la DB
         return res.status(200).json(proyectoGuardado)   
@@ -19,22 +19,89 @@ const nuevoProyecto = async (req, res) => {
 }
 
 // trae los proyectos del usuario autenticado
-const obtenerProyectos = async (req, res) => { 
+const obtenerProyectos = async (req, res) => {
 
-    console.log('desde obtener proyectos')
-
-    // req.proyectos = await Proyectos.findAll()
-    // res.status(200).json(req.proyectos)
+    //traemos los proyectos que pertenecen al usuario autenticado
+    const proyectos = await Proyecto.find().where('creador').equals(req.usuario) 
+    return res.status(200).json(proyectos)
+    
 }
 
 // obtiene un proyecto por id del usuario autenticado 
-const obtenerProyecto = async (req, res) => { }
+const obtenerProyecto = async (req, res) => {
+    
+    // tomamos el id del routing dinamico
+    const { id } = req.params
+
+    try {
+        //traemos un proyecto que pertenecen al usuario autenticado
+        const proyectoObtenido = await Proyecto.findById(id) //.where('creador').equals(req.usuario) 
+
+        // if (!proyectoObtenido) {
+        //     const error = new Error('Proyecto no encontrado.')
+        //     return res.status(404).json({ msg: error.message })
+        // }
+
+        if (proyectoObtenido.creador.toString() !== req.usuario._id.toString()) {
+            const error = new Error('Solo el creador o colaborador puede obtener un proyecto.')
+            return res.status(401).json({ msg: error.message })
+        }
+
+        // response con el proyecto
+        return res.status(200).json(proyectoObtenido)
+        
+    } catch (error) {
+        return res.status(404).json({ msg: 'Proyecto no encontrado.' })
+    }
+
+ }
 
 // editar los datos del proyecto
-const editarProyecto = async (req, res) => { }
+const editarProyecto = async (req, res) => {
+
+    const { id } = req.params
+    try {
+
+        const proyectoObtenido = await Proyecto.findById(id) 
+
+        if (proyectoObtenido.creador.toString() !== req.usuario._id.toString()) {
+            const error = new Error('Solo el creador puede editar un proyecto.')
+            return res.status(401).json({ msg: error.message })
+        }
+
+        proyectoObtenido.nombre = req.body.nombre || proyectoObtenido.nombre
+        proyectoObtenido.descripcion = req.body.descripcion || proyectoObtenido.descripcion
+        proyectoObtenido.fechaEntrega = req.body.fechaEntrega || proyectoObtenido.fechaEntrega
+        proyectoObtenido.cliente = req.body.cliente || proyectoObtenido.cliente
+
+        const proyectoAlmacenado = await proyectoObtenido.save()
+        return res.status(200).json(proyectoAlmacenado)
+    
+    } catch (error) {
+        return res.status(404).json({ msg: error.message })
+    }
+ }
 
 // elimina un proyecto existente
-const eliminarProyecto = async (req, res) => { }
+const eliminarProyecto = async (req, res) => {
+    
+    const { id } = req.params
+    try {
+        const proyectoObtenido = await Proyecto.findById(id) 
+
+        if (proyectoObtenido.creador.toString() !== req.usuario._id.toString()) {
+            const error = new Error('Solo el creador puede eliminar un proyecto.')
+            return res.status(401).json({ msg: error.message })
+        }
+
+        await proyectoObtenido.deleteOne()
+        return res.status(204).json({ msg: "Proyecto eliminador con éxito."}) // 204 No Content (Se eliminó correctamente y no devuelve contenido)
+
+    } catch (error) {
+        return res.status(404).json({ msg: 'Proyecto no encontrado.' })
+    }
+
+ }
 
 // agregar un colaborador existente al proyecto
 const agregarColaborador = async (req, res) => { }
