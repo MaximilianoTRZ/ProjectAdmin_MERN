968190b6fab25bd72b40c7cd33e38fd9032df988
MaximilianoTRZ <maximilianoriverozuin@gmail.com>
2023-04-06 16:28:53 -0300

    singup form created

--- frontend/src/pages/Registrar.jsx
+++ frontend/src/pages/Registrar.jsx
@@ -14,6 +14,18 @@ const Registrar = () => {
       <div className='my-5'>
         <label 
           className='uppercase text-gray-600 block text-xl font-bold' 
+          htmlFor='name'
+        >Name</label>
+        <input 
+          id='name'
+          type="name" 
+          placeholder='your name' 
+          className='w-full mt-3 p-3 border border-slate-400 rounded-xl bg-gray-200'
+        />
+      </div>
+      <div className='my-5'>
+        <label 
+          className='uppercase text-gray-600 block text-xl font-bold' 
           htmlFor='email'
         >Email</label>
         <input 
@@ -35,21 +47,29 @@ const Registrar = () => {
           className='w-full mt-3 p-3 border border-slate-400 rounded-xl bg-gray-200'
         />
       </div>
+      <div className='my-5'>
+        <label 
+          className='uppercase text-gray-600 block text-xl font-bold' 
+          htmlFor='password2'
+        >Repeat Password</label>
+        <input 
+          id='password2'
+          type="password" 
+          placeholder='your password again' 
+          className='w-full mt-3 p-3 border border-slate-400 rounded-xl bg-gray-200'
+        />
+      </div>
 
       <nav className='lg:flex lg:justify-between'>
         <Link
           className='block text-center my-5 text-slate-500 uppercase text-sm'
-          to="registrar"
-        >Don't have an account? Sing Up</Link>
-        <Link
-          className='block text-center my-5 text-slate-500 uppercase text-sm'
-          to="olvide-password"
-        >Forgot my password</Link>
+          to="/"
+        >Already have an account? Sing In</Link>
       </nav>
 
       <input 
         type="submit" 
-        value="Sing In"
+        value="Sing Up"
         className='bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded-xl 
         hover:cursor-pointer hover:bg-sky-800 transition-colors'   
       />
